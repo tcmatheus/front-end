1f28169ec7718fba0ef1ff90f05077c6f34491e0
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-03 09:19:44 -0300

    Fix: Atualizando rotas

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/loginPage";
 import DashboardPage from "./pages/dashboard";
+import EscolhaUsuario from "./pages/escolhaUsuario";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage tipoLogin={"Fornecedor"} />} />
+        <Route path="/" element={<EscolhaUsuario/>} />
+        <Route path="/loginFornecedor" element={<LoginPage tipoLogin={"Fornecedor"} />} />
+        <Route path="/loginVendedor" element={<LoginPage tipoLogin={"Vendedor"} />} />
         <Route path="/dashboard" element={<DashboardPage/>} />
       </Routes>
     </Router>
