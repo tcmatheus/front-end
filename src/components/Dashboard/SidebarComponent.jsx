d27157ee47d0d483d619f90990e5c38679f51b82
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-03 09:22:07 -0300

    Fix: Ajustando as rotas dos itens do menu lateral

--- src/components/Dashboard/SidebarComponent.jsx
+++ src/components/Dashboard/SidebarComponent.jsx
@@ -7,18 +7,21 @@ import '../../styles/Dashboard/sidebar.css';
 
 export default function SidebarComponent() {
     const items = [
-        { label: 'Dashboard', icon: 'pi pi-fw pi-home', command: () => { window.location.hash = "/dashboard"; }},
-        { label: 'Orders', icon: 'pi pi-fw pi-file', command: () => { window.location.hash = "/orders"; }},
-        { label: 'Products', icon: 'pi pi-fw pi-list', command: () => { window.location.hash = "/products"; }},
-        { label: 'Customers', icon: 'pi pi-fw pi-users', command: () => { window.location.hash = "/customers"; }},
-        { label: 'Reports', icon: 'pi pi-fw pi-chart-bar', command: () => { window.location.hash = "/reports"; }},
-        { label: 'Settings', icon: 'pi pi-fw pi-cog', command: () => { window.location.hash = "/settings"; }}
+        { label: 'Dashboard', icon: 'pi pi-fw pi-home', url:"/dashboard"},
+        { label: 'Produtos', icon: 'pi pi-fw pi-box', url:"/produtos"},
+        { label: 'Pedidos', icon: 'pi pi-fw pi-receipt', url:"/pedidos"},
+        { label: 'Minha Integração', icon: 'pi pi-fw pi-objects-columns', url:"/minhaIntegracao"},
+        { label: 'Comissões', icon: 'pi pi-fw pi-dollar', url:"/comissoes"},
+        { label: 'Vendedores', icon: 'pi pi-fw pi-users', url:"/vendedores"},
+        { label: 'Configuracões', icon: 'pi pi-fw pi-cog', url:"/configuracoes"},
+        { label: 'Ranking', icon: 'pi pi-fw pi-sort-up-fill', url:"/rankings"},
+        { label: 'Sair', icon: 'pi pi-fw pi-sign-out', url:"/"}
     ];
 
     return (
         <aside className="custom-sidebar">
-            <Image className="container-loginFields-logo" src="../assets/logos/logoSouDrop.png" alt="SouDrop Logo" width="80" />
-            <Menu model={items} />
+            <Image className="container-loginFields-logo" src="../assets/logos/logoSouDrop.png" alt="SouDrop Logo" width="100" />
+            <Menu className='backgroundMenuSidebar' model={items} />
         </aside>
     );
 }
