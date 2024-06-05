a433812b46ec194516c355414730ef3c39e8cfa6
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-05 11:16:09 -0300

    Feat: Criação da seção de Destaque de produtos

---
+++ src/components/Dashboard/Destaques/DestaquesLista.jsx
@@ -0,0 +1,44 @@
+import DestaqueProduto from "../Destaques/DestaqueProduto";
+
+export default function DestaquesLista() {
+
+const produtos = [
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 10, precoVenda: 20 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 15, precoVenda: 25 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    { imagem: "https://www.picsum.photos/1000", precoCusto: 12, precoVenda: 22 },
+    // Add more products here
+];
+
+  return (
+    <div className="destaquesConjunto">
+      <div className="scrollContainer">
+        {produtos.map((produto) => {
+              return <DestaqueProduto produto={produto}/>
+        })}
+      </div>
+    </div>
+  );
+}
