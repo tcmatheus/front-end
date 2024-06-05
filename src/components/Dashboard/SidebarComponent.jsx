b97a2f42fd95702d826b5e3563ab1df4df4b7939
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-05 10:49:03 -0300

    Fix: Atualizando o ícone da sidebar

--- src/components/Dashboard/SidebarComponent.jsx
+++ src/components/Dashboard/SidebarComponent.jsx
@@ -6,11 +6,12 @@ import { Image } from 'primereact/image';
 import '../../styles/Dashboard/sidebar.css';
 
 export default function SidebarComponent() {
+
     const items = [
         { label: 'Dashboard', icon: 'pi pi-fw pi-home', url:"/dashboard"},
         { label: 'Produtos', icon: 'pi pi-fw pi-box', url:"/produtos"},
         { label: 'Pedidos', icon: 'pi pi-fw pi-receipt', url:"/pedidos"},
-        { label: 'Minha Integração', icon: 'pi pi-fw pi-objects-columns', url:"/minhaIntegracao"},
+        { label: 'Minha Integração', icon: 'pi pi-fw pi-slack', url:"/minhaIntegracao"},
         { label: 'Comissões', icon: 'pi pi-fw pi-dollar', url:"/comissoes"},
         { label: 'Vendedores', icon: 'pi pi-fw pi-users', url:"/vendedores"},
         { label: 'Configuracões', icon: 'pi pi-fw pi-cog', url:"/configuracoes"},
