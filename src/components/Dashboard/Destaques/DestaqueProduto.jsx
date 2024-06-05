56824887b0c608042cd6b68b346d7a50402051fc
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-05 11:16:41 -0300

    Feat: Criação do produto destaque

--- src/components/Dashboard/Destaques/DestaqueProduto.jsx
+++ src/components/Dashboard/Destaques/DestaqueProduto.jsx
@@ -1,11 +1,28 @@
-import '../../../styles/Dashboard/Destaques/destaques.css'
+import { Dialog } from "primereact/dialog";
+import "../../../styles/Dashboard/Destaques/destaques.css";
+import { useState } from "react";
 
-export default function DestaqueProduto(produto) {
+export default function DestaqueProduto({ produto }) {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div className="destaquesConjunto">
-      <img src={produto.image}></img>
-      <p>Preço de Custo: ${produto.precoCusto}</p>
-      <p>Preço de Venda: ${produto.precoVenda}</p>
+      <div className="destaqueProduto" onClick={() => setIsVisible(true)}>
+        <img width={100} src={produto.imagem}></img>
+        <p>Preço de Custo: ${produto.precoCusto}</p>
+        <p>Preço de Venda: ${produto.precoVenda}</p>
+      </div>
+      <Dialog draggable={false}
+        visible={isVisible}
+        onHide={() => {
+          if (!isVisible) return;
+          setIsVisible(false);
+        }}
+      >
+        <img width={500} src={produto.imagem}></img>
+        <p>Preço de Custo: ${produto.precoCusto}</p>
+        <p>Preço de Venda: ${produto.precoVenda}</p>
+      </Dialog>
     </div>
   );
 }
