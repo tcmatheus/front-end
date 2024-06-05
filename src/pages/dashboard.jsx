dc0c321770d612d20d6624e7b9864df464403bea
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-05 18:10:15 -0300

    Fix: Ajustando icones e botão de redirecionamento

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -5,14 +5,14 @@ import DestaquesLista from "../components/Dashboard/Destaques/DestaquesLista";
 
 import "../styles/Dashboard/dashboardPage.css";
 import "../styles/Dashboard/card.css";
+import { Button } from "primereact/button";
+import { Link } from "react-router-dom";
 
 export default function DashboardPage() {
   return (
     <div className="dashboardContainer">
       <div className="dashboardlayout">
-        <SidebarComponent/>
         <div className="dashboardContent">
-          <Navbar />
           <div className="cardGroupContainer">
             <Card
               texto={"Total de Vendas Hoje"}
@@ -24,29 +24,31 @@ export default function DashboardPage() {
                 <Card
                   texto={"Vendas esse Mês"}
                   valor={"R$1.000,00"}
-                  image={"https://www.picsum.photos/60"}
+                  image={"./assets/icons/Graph.png"}
                 />
                 <Card
                   texto={"Ativos"}
                   valor={"R$1.000,00"}
                   tipo={"secondary"}
-                  image={"https://www.picsum.photos/60"}
+                  image={"./assets/icons/Chart.png"}
                 />
                 <Card
                   texto={"Clientes"}
                   valor={"R$1.000,00"}
-                  image={"https://www.picsum.photos/60"}
+                  image={"./assets/icons/Clients.png"}
                 />
                 <Card
                   texto={"Comissões"}
                   valor={"R$1.000,00"}
-                  image={"https://www.picsum.photos/60"}
+                  image={"./assets/icons/Icon.png"}
                 />
                 <div className="cardGroup">
                   <Card
                     texto={"Chamados"}
                     paragrafo={"Mais detalhes"}
                     tipo={"forth"}
+                    tagUm={"Alta"}
+                    tagDois={"Media"}
                   />
                 </div>
               </div>
@@ -59,7 +61,13 @@ export default function DashboardPage() {
             />
           </div>
 
-          <h1 className="destaquesTitle">Produtos em destaque</h1>
+          <div className="destaque__container">
+              <h1 className="destaquesTitle">Produtos em destaque</h1>
+            <Link to={'produtos'} >
+              <Button label="Ver Mais Produtos"/>
+            </Link>
+          </div>
+
           <DestaquesLista />
         </div>
       </div>
