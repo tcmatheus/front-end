c3679454c6a4acaa16420312bf3900d025250c19
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-07 11:28:47 -0300

    Fix: Alterações na página de produtos

--- src/pages/produtosPage.jsx
+++ src/pages/produtosPage.jsx
@@ -1,12 +1,28 @@
+import { Button } from "primereact/button";
 import DestaquesLista from "../components/Dashboard/Destaques/DestaquesLista";
 
-import '../styles/produtosPage.css';
+import "../styles/produtosPage.css";
+import ModalProduto from "../components/ModalProduto";
+import { useState } from "react";
 
 export default function ProdutosPage() {
-    return(
-        <div className="produtosContainer">
-            <h1>Produtos em Destaque</h1>
-            <DestaquesLista/>
-        </div>
-    )
+  const userType = localStorage.getItem("userType");
+
+  const [isVisible, setIsVisible] = useState(false);
+
+return (
+    <div className="produtosContainer">
+        <h1>Produtos em Destaque</h1>
+        {userType === "Vendedor" && <DestaquesLista />}
+        {userType === "Fornecedor" && (
+            <>
+                <Button
+                    onClick={() => setIsVisible(!isVisible)}
+                    label="Cadastrar Produto"
+                />
+                {isVisible === true && <ModalProduto isVisible={isVisible} />}
+            </>
+        )}
+    </div>
+);
 }
