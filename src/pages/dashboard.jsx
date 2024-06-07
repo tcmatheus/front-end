2cda546e42ed8b022e07c1fd13003d0e9d57b6a0
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-07 11:25:50 -0300

    Fix: Dashboard agora renderiza de acordo com o tipo de Usuario

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,9 @@
-import Card from "../components/Dashboard/card";
-import DestaquesLista from "../components/Dashboard/Destaques/DestaquesLista";
-
 import "../styles/Dashboard/dashboardPage.css";
 import "../styles/Dashboard/card.css";
 import "../styles/Dashboard/Destaques/destaques.css";
 
-import { Button } from "primereact/button";
-import { Link } from "react-router-dom";
+import DashboardVendedor from "../components/Dashboard/dashboardVendedor";
+import DashboardFornecedor from "../components/Dashboard/dashboardFornecedor";
 
 export default function DashboardPage() {
 
@@ -14,113 +11,8 @@ export default function DashboardPage() {
 
   return (
     <div className="dashboardContainer">
-      {userType === "Vendedor" && <div className="dashboardlayout">
-        <div className="dashboardContent">
-          <div className="cardGroupContainer">
-            <Card
-              texto={"Total de Vendas Hoje"}
-              paragrafo={"do dia 28 Máio 2024, 09:41 PM"}
-              tipo={"third"}
-            />
-            <div>
-              <div className="cardGroup">
-                <Card
-                  texto={"Vendas esse Mês"}
-                  valor={"R$1.000,00"}
-                  image={"./assets/icons/Graph.png"}
-                />
-                <Card
-                  texto={"Ativos"}
-                  valor={"R$1.000,00"}
-                  tipo={"secondary"}
-                  image={"./assets/icons/Chart.png"}
-                />
-                <Card
-                  texto={"Clientes"}
-                  valor={"R$1.000,00"}
-                  image={"./assets/icons/Clients.png"}
-                />
-                <Card
-                  texto={"Comissões"}
-                  valor={"R$1.000,00"}
-                  image={"./assets/icons/Icon.png"}
-                />
-                <div className="cardGroup">
-                  <Card
-                    texto={"Chamados"}
-                    paragrafo={"Mais detalhes"}
-                    tipo={"forth"}
-                    tagUm={"Alta"}
-                    tagDois={"Media"}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container_banner">
-            <img class="banner" src="https://www.picsum.photos/2000/400" />
-          </div>
-
-          <div className="destaque__container">
-            <h1 className="destaquesTitle">Produtos em destaque</h1>
-            <Link to={"produtos"}>
-              <Button label="Ver Mais Produtos" />
-            </Link>
-          </div>
-          <div className="destaquesContainer">
-            <DestaquesLista />
-          </div>
-        </div>
-      </div>}
-
-
-
-
-      {userType === "Fornecedor" && <div className="dashboardlayout">
-        <div className="dashboardContent">
-          <div className="cardGroupContainer">
-            <Card
-              texto={"Total de Vendas Hoje"}
-              paragrafo={"do dia 28 Máio 2024, 09:41 PM"}
-              tipo={"third"}
-            />
-            <div>
-              <div className="cardGroup">
-                <Card
-                  texto={"Vendas esse Mês"}
-                  valor={"R$1.000,00"}
-                  image={"./assets/icons/Graph.png"}
-                />
-                <Card
-                  texto={"Ativos"}
-                  valor={"R$1.000,00"}
-                  tipo={"secondary"}
-                  image={"./assets/icons/Chart.png"}
-                />
-                <Card
-                  texto={"Clientes"}
-                  valor={"R$1.000,00"}
-                  image={"./assets/icons/Clients.png"}
-                />
-                <Card
-                  texto={"Comissões"}
-                  valor={"R$1.000,00"}
-                  image={"./assets/icons/Icon.png"}
-                />
-                <div className="cardGroup">
-                  <Card
-                    texto={"Chamados"}
-                    paragrafo={"Mais detalhes"}
-                    tipo={"forth"}
-                    tagUm={"Alta"}
-                    tagDois={"Media"}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>}
+      {userType === "Vendedor" && <DashboardVendedor/>}
+      {userType === "Fornecedor" && <DashboardFornecedor/>}
     </div>
   );
 }
