91f293812a0bdc73aa797b70f1009b96437cc371
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-21 17:13:31 -0300

    Fix: Prettier

--- src/components/Dashboard/card.jsx
+++ src/components/Dashboard/card.jsx
@@ -2,7 +2,16 @@ import "../../styles/Dashboard/card.css";
 import StackedBarChart from "./Charts/stackedBarChart";
 import PriorityCard from "./priorityCard";
 
-export default function Card({ texto, tipo, valor, paragrafo, image, tagUm, tagDois, hasChart }) {
+export default function Card({
+  texto,
+  tipo,
+  valor,
+  paragrafo,
+  image,
+  tagUm,
+  tagDois,
+  hasChart,
+}) {
   return (
     <div
       className={
@@ -15,16 +24,43 @@ export default function Card({ texto, tipo, valor, paragrafo, image, tagUm, tagD
           : "card-container"
       }
     >
-      <div>
-        <p className="cardContentTitle">{texto}</p>
-        <h2 className="cardContentValor">{valor}</h2>
-        <h3 className="">{paragrafo}</h3>
-        {hasChart === true && <StackedBarChart/>}
+      <div style={{ height: "100%" }}>
+        <div>
+          <p className="cardContentTitle">{texto}</p>
+          <h2 className="cardContentValor">{valor}</h2>
+          <h3 className="">{paragrafo}</h3>
+          {hasChart === true && (
+          <div className="chartCard__container">
+            <StackedBarChart />
+          </div>
+        )}
+        </div>
+        {/* <div className="chartCard__container--infoCards">
+          <div className="chartCard__container--infoCards-card">
+            <p>Vendidos</p>
+            <h2>449</h2>
+          </div>
+          <div className="chartCard__container--infoCards-card">
+            <p>Recebidos</p>
+            <h2>426</h2>
+          </div>
+          <div className="chartCard__container--infoCards-card">
+            <p>Tempo de Resposta</p>
+            <h2>33m</h2>
+          </div>
+          <div className="chartCard__container--infoCards-card">
+            <p>Última Atualização</p>
+            <h2>3h 8m</h2>
+          </div>
+        </div> */}
       </div>
       <div className="priorityCard__container">
-        {tagUm === "Baixa" || tagDois === "Baixa" && <PriorityCard tags={["Baixa", "Baixa"]} />}
-        {tagUm === "Media" || tagDois === "Media" && <PriorityCard tags={["Media", "Media"]} />}
-        {tagUm === "Alta" || tagDois === "Alta" && <PriorityCard tags={["Alta", "Alta"]} />}
+        {tagUm === "Baixa" ||
+          (tagDois === "Baixa" && <PriorityCard tags={["Baixa", "Baixa"]} />)}
+        {tagUm === "Media" ||
+          (tagDois === "Media" && <PriorityCard tags={["Media", "Media"]} />)}
+        {tagUm === "Alta" ||
+          (tagDois === "Alta" && <PriorityCard tags={["Alta", "Alta"]} />)}
       </div>
       <img src={image} alt="" width={60} />
     </div>
