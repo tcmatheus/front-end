40138a29be8eb2f009fb49423ff8dd1593772b77
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-25 11:25:40 -0300

    Feat (Firebase): Login Feature

--- src/services/Login/loginService.js
+++ src/services/Login/loginService.js
@@ -1,10 +1,15 @@
-import firebase from "../../firebase-config"
+import { auth } from '../../firebase-config';
 
 export const loginUser = async (email, password) => {
   try {
-    const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+    const userCredential = await auth.signInWithEmailAndPassword(email, password);
     console.log("Login bem-sucedido:", userCredential.user);
   } catch (error) {
     console.error("Erro no login:", error.message);
+    throw error;  // Certifique-se de propagar ou tratar o erro apropriadamente
   }
+};
+
+export const logoutUser = () => {
+  return auth.signOut();
 };
