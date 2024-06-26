2b6663309dc04d3e11bfd4aae87be821657a6310
LuizDanielOliveira <113204394+LuizDanielOliveira@users.noreply.github.com>
2024-06-26 17:45:06 -0300

    Update CSS

--- src/components/Login/registerUser.jsx
+++ src/components/Login/registerUser.jsx
@@ -7,7 +7,9 @@ import { registerUser } from "../../services/Login/registerService";
 import { loginUser } from "../../services/Login/loginService";
 import { useNavigate } from "react-router-dom";
 import { Dropdown } from "primereact/dropdown";
+import '../../styles/Login/registerUser.css';
 import { db } from "../../firebase-config";
+
 
 export default function RegisterUser() {
   const [visible, setVisible] = useState(false);
@@ -48,46 +50,59 @@ export default function RegisterUser() {
       />
       <Dialog
       draggable={false}
-        header="Registre-se!"
+      className="nameregistrar"
+        header="Registrar-se"
         visible={visible}
-        style={{ width: "70vw", height: "60vh" }}
+        style={{ width: "44vw", height: "60vh",}}
         onHide={() => {
           if (!visible) return;
           setVisible(false);
         }}
       >
-        <FloatLabel>
+        <FloatLabel 
+        className="emailbox">
           <InputText
+          unstyled={true}
+          className="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor="email">Email</label>
         </FloatLabel>
-        <FloatLabel>
+        <FloatLabel
+        className="senhabox">
           <InputText
+          unstyled={true}
+          className="senha"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <label htmlFor="password">Senha</label>
         </FloatLabel>
-        <Button
-          label="Registrar!"
-          icon="pi pi-verified"
-          onClick={() => handleRegister()}
-        ></Button>
         <Dropdown
+        className="dropdown"
           value={selectedUserType}
           onChange={(e) => setSelectedUserType(e.value)}
           options={userType}
           optionLabel="name"
           placeholder="Selecione o tipo de usuário"
-          className="w-full md:w-14rem"
           checkmark={true}
           highlightOnSelect={false}
         />
+        <Button
+        unstyled={true}
+        className="registrar"
+          label="Registrar!"
+          
+          onClick={() => handleRegister()}
+        ></Button>
+        
+        
       </Dialog>
+
+      
     </div>
   );
 }
