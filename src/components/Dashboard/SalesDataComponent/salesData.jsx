82e6fb41c2a5ef86c47b259a970ecd4b4973c405
João Henrique Batista <404joaohbatista@gmail.com>
2024-06-26 16:30:58 -0300

    feature(screen responsiblitity): adjust size

---
+++ src/components/Dashboard/SalesDataComponent/salesData.jsx
@@ -0,0 +1,18 @@
+import "../../../styles/Dashboard/salesData.css";
+
+export default function SalesData({
+  texto,
+  valor,
+  hasChart,
+  tipo,
+  image,
+  background,
+  colorTitle,
+  colorValue,
+}) {
+  return (
+    <div className="salesContainer">
+      <p>Teste</p>
+    </div>
+  );
+}
--- src/components/Dashboard/dashboardFornecedor.jsx
+++ src/components/Dashboard/dashboardFornecedor.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "./card";
+import SalesData from "./SalesDataComponent/salesData";
+import SalesProducts from "./SalesProductsComponent/salesProducts.jsx";
 import "../../styles/Dashboard/dashboardPage.css";
 import "../../styles/Dashboard/card.css";
 import Ranking from "./Ranking/ranking.jsx";
@@ -11,40 +13,48 @@ export default function DashboardFornecedor() {
       <div className="dashboardContent">
         <div className="cardGroupContainer">
           {/* Total Vendas*/}
-
-          {/* Container Cards*/}
-          <div className="cardGroup">
-            <Card
-              tipo={"commun"}
-              texto={"Vendas esse Mês"}
-              valor={"$682.5"}
-              image={"./assets/icons/Charts.png"}
-              background={"#FFFFFF"}
-            />
-            <Card
-              tipo={"commun"}
-              texto={"Ativos"}
-              valor={"$540.50"}
-              image={"./assets/icons/Chart.png"}
-              background={"linear-gradient(to bottom right, #4caf50, #1e693b)"}
-              colorTitle={"#E9EDF7"}
-              colorValue={"#FFFFFF"}
-            />
-            <Card
-              tipo={"image"}
-              image={"./assets/icons/Ml.png"}
-              background={"#FED33A"}
-            />
-            <Card
-              tipo={"commun"}
-              texto={"Comissões"}
-              valor={"$350.40"}
-              image={"./assets/icons/Icon.png"}
-              background={"#FFFFFF"}
-            />
+          <div className="salesGroup">
+            <SalesData />
           </div>
 
-          {/* Container Vendas*/}
+          <div className="rightComponents">
+            {/* Container Cards*/}
+            <div className="cardGroup">
+              <Card
+                tipo={"commun"}
+                texto={"Vendas esse Mês"}
+                valor={"$682.5"}
+                image={"./assets/icons/Charts.png"}
+                background={"#FFFFFF"}
+              />
+              <Card
+                tipo={"commun"}
+                texto={"Ativos"}
+                valor={"$540.50"}
+                image={"./assets/icons/Chart.png"}
+                background={
+                  "linear-gradient(to bottom right, #4caf50, #1e693b)"
+                }
+                colorTitle={"#E9EDF7"}
+                colorValue={"#FFFFFF"}
+              />
+              <Card
+                tipo={"image"}
+                image={"./assets/icons/Ml.png"}
+                background={"#FED33A"}
+              />
+              <Card
+                tipo={"commun"}
+                texto={"Comissões"}
+                valor={"$350.40"}
+                image={"./assets/icons/Icon.png"}
+                background={"#FFFFFF"}
+              />
+            </div>
+
+            {/* Container Vendas*/}
+            <SalesProducts />
+          </div>
         </div>
         {/* <div className="inferior__container">
           <Ranking />
