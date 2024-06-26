177f69deb64b0c605d8159252d8db3133d6dcbf2
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-26 17:28:37 -0300

    Fix: Removendo o uso do localStorage

--- src/components/Dashboard/navbar.jsx
+++ src/components/Dashboard/navbar.jsx
@@ -3,12 +3,10 @@ import '../../styles/Dashboard/navbar.css'
 import {Avatar} from 'primereact/avatar'
 export default function Navbar() {
 
-    const userType = localStorage.getItem('userType');
-
 return (
     <div className="navbar">
         <div className='navbar-container'>
-            <h1>Visão Geral - {userType}</h1>
+            <h1>Visão Geral</h1>
             <div className='navbar-profile'>
                 <i className="pi pi-bell"></i>
                 <p>Lucas Almada</p>
