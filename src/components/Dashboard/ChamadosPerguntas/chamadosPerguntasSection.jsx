e56834779e07d4b5fab6bd80e5bdb0b998bcb278
João Henrique Batista <404joaohbatista@gmail.com>
2024-06-27 17:24:18 -0300

    feature(ranking, questions): adjust design of question and ranking

--- src/components/Dashboard/ChamadosPerguntas/chamadosPerguntasSection.jsx
+++ src/components/Dashboard/ChamadosPerguntas/chamadosPerguntasSection.jsx
@@ -2,14 +2,16 @@ import ChamadosPerguntas from "./chamadosPerguntas";
 
 import "../../../styles/Dashboard/ChamadosPerguntas/chamadosPerguntas.css";
 
-export default function ChamadosPerguntasSection(){
-    return(
-        <div className="chamadosPerguntasSection__container">
-            <h2 className="chamadosPerguntasSection__container--title">Chamados e Perguntas</h2>
-            <ChamadosPerguntas tag={"Atraso"}/>
-            <ChamadosPerguntas tag={"Atencao"}/>
-            <ChamadosPerguntas tag={"Atendido"}/>
-            <ChamadosPerguntas tag={"Atendido"}/>   
-        </div>
-    )
+export default function ChamadosPerguntasSection() {
+  return (
+    <div className="chamadosPerguntasSection__container">
+      <h2>Chamados e Perguntas</h2>
+      <div className="chamadosPerguntasSection__container--chamados">
+        <ChamadosPerguntas tag={"Atraso"} />
+        <ChamadosPerguntas tag={"Atencao"} />
+        <ChamadosPerguntas tag={"Atendido"} />
+        <ChamadosPerguntas tag={"Atendido"} />
+      </div>
+    </div>
+  );
 }
