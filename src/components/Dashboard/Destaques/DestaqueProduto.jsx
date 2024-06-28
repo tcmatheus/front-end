d415a03e1d7eb99bcfacab51803e30c2ea8e2d53
Lucas Almada de Andrade <lucas.almada6@yahoo.com.br>
2024-06-28 11:56:03 -0300

    Fix: Modal abrindo sem bug de não poder abrir novamente

--- src/components/Dashboard/Destaques/DestaqueProduto.jsx
+++ src/components/Dashboard/Destaques/DestaqueProduto.jsx
@@ -4,19 +4,29 @@ import ModalProduto from "../../CadastrarProduto/modalProduto";
 
 export default function DestaqueProduto({ produto }) {
   const [isVisible, setIsVisible] = useState(false);
+
+  const toggleModal = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const closeModal = (e) => {
+    if (e.target.classList.contains('modalBackdrop')) {
+      toggleModal();
+    }
+  };
+
   return (
     <div className="destaquesConjunto">
-      <div className="destaqueProduto" onClick={() => setIsVisible(true)}>
-        <img width={100} src={produto?.imagemURL}></img>
+      <div className="destaqueProduto" onClick={toggleModal}>
+        <img width={100} src={produto.imagemURL}></img>
         <h3 className="destaqueProduto__nome">{produto.nome}</h3>
         <p className="destaqueProduto__nome">Preço: R${produto.preco}</p>
-        
-        {/* <p className="destaqueProduto__custo">Preço de Custo: {produto.precoCusto}</p> */}
-        {/* <p className="destaqueProduto__venda">Preço de Venda: {produto.precoVenda}</p> */}
       </div>
-      <div onClick={() => setIsVisible(true)}>
-        {isVisible === true && <ModalProduto isVisible={isVisible} produto={produto}/>}
-      </div>
+      {isVisible && (
+        <div className="modalBackdrop" onClick={closeModal}>
+          <ModalProduto isVisible={isVisible} produto={produto} onClose={closeModal} />
+        </div>
+      )}
     </div>
   );
 }
