ee8d3152d62829f85e339d34ff114bce2a724f56
LuizDanielOliveira <113204394+LuizDanielOliveira@users.noreply.github.com>
2024-07-04 14:54:50 -0300

    Updates de Carrossel na Dashboard de vendedor e CSS no modal Dialog de informação de Pedidos

--- src/components/Dashboard/StatusPedidos/modalPedidos.jsx
+++ src/components/Dashboard/StatusPedidos/modalPedidos.jsx
@@ -2,10 +2,10 @@ import { Dialog } from "primereact/dialog";
 import { useEffect, useState } from "react";
 import { getPedidoById } from "../../../pages/PagesService/pedidosService";
 import { formatDate } from "../../../common/formatDate";
+import "./ModalPedidos.css";
 
 export default function ModalPedidos({ isVisible, pedido, onClose }) {
   const [selectedPedido, setSelectedPedido] = useState(null);
- // const [produto, setProduto] = useState(null);
 
   useEffect(() => {
     const getUmPedido = async () => {
@@ -15,39 +15,43 @@ export default function ModalPedidos({ isVisible, pedido, onClose }) {
       }
     };
 
-    // const pegaProduto = async () => {
-    //     if (selectedPedido){
-    //         const product = await getPedidoById(selectedPedido.itens[0].produtoId);
-    //         setProduto(product.data);
-    //     }
-    // }
-
     getUmPedido();
-    // pegaProduto();
   }, [pedido]);
 
   return (
     <Dialog
+      className="custom-modal"
       blockScroll={true}
       draggable={false}
       visible={isVisible}
-      style={{ width: "80vw", height: "100vh" }}
+      style={{ width: "80vw", height: "auto" }}
       modal={true}
       onHide={onClose}
     >
       {selectedPedido && (
-        <div>
-          <p>Numero do pedido: {selectedPedido.numero}</p>
-          <p>Produto: {selectedPedido.itens[0].descricao}</p>
-          <p>Quantidade: {selectedPedido.itens[0].quantidade}</p>
-          <p>Total do Pedido: R$ {selectedPedido.total}</p>
-          <p>Data de Venda: {formatDate(selectedPedido.data)}</p>
-          <p>Data Prevista: {formatDate(selectedPedido.dataPrevista)}</p>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h2>Número do pedido: {selectedPedido.numero}</h2>
+            <h3>Data do Pedido: {formatDate(selectedPedido.data)}</h3>
+            <h3>Total do Pedido: R$ {selectedPedido.total}</h3>
+            <button onClick={onClose} className="close-button">X</button>
+          </div>
+          <div className="modal-body">
+            {selectedPedido.itens.map((item, index) => (
+              <div key={index} className="modal-item">
+                <img src={item.produto.imagem} alt={item.produto.nome} />
+                <div className="item-details">
+                  <h3>{item.produto.nome}</h3>
+                  <p>Vendas na última semana: {item.produto.vendas}</p>
+                  <p>Popularidade: {item.produto.popularidade} ★</p>
+                  <p>Preço de Compra: R$ {item.precoCompra}</p>
+                  <p>Preço de Venda: R$ {item.precoVenda}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       )}
-      {/* <StatusPedidosCard pedido={pedido} /> */}
     </Dialog>
   );
-
-
 }
--- src/pages/pedidosPage.jsx
+++ src/pages/pedidosPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import StatusPedidosSection from "../components/Dashboard/StatusPedidos/statusPedidosSection";
 import "../styles/pedidosPage.css";
 import ModalPedidos from "../components/Dashboard/StatusPedidos/modalPedidos";
@@ -9,8 +9,6 @@ export default function PedidosPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPedido, setSelectedPedido] = useState(null);
   const [pedidos, setPedidos] = useState([]);
-
-  // const [produto, setProduto] = useState(null);
 
   const handleClickModal = (pedido) => {
     setSelectedPedido(pedido);
@@ -32,44 +30,30 @@ export default function PedidosPage() {
       }
     };
 
-    
-    // const pegaProduto = async () => {
-    //     if (selectedPedido){
-    //         const product = await getProdutoById(selectedPedido.itens[0].produto.id);
-    //         setProduto(product.data);
-    //     }
-    // }
-
-
     fetchPedidos();
-    // pegaProduto()
   }, []);
 
   return (
     <section className="pedidos__container">
       <h1>Pedidos</h1>
-      <>
-        <StatusPedidosSection />
-        <div>
-          {pedidos.map((pedido) => (
-            <div key={pedido.numero} onClick={() => handleClickModal(pedido)}>
-              {/* <p>{produto}</p> */}
-              <p>Pedido: {pedido.numero}</p>
-              <p>Data de Venda: {formatDate(pedido.data)}</p>
-              <p>Data Prevista: {formatDate(pedido.dataPrevista)}</p>
-              <p>Total do pedido: R$ {pedido.total}</p>
-            </div>
-          ))}
-        </div>
-        {selectedPedido && (
-          <ModalPedidos
-            pedido={selectedPedido}
-            isVisible={isModalOpen}
-            onClose={handleCloseModal}
-          />
-        )}
-        {/* <Ranking /> */}
-      </>
+      <StatusPedidosSection />
+      <div>
+        {pedidos.map((pedido) => (
+          <div key={pedido.numero} className="pedido__item" onClick={() => handleClickModal(pedido)}>
+            <p>Pedido: {pedido.numero}</p>
+            <p>Data de Venda: {formatDate(pedido.data)}</p>
+            <p>Data Prevista: {formatDate(pedido.dataPrevista)}</p>
+            <p>Total do pedido: R$ {pedido.total}</p>
+          </div>
+        ))}
+      </div>
+      {selectedPedido && (
+        <ModalPedidos
+          pedido={selectedPedido}
+          isVisible={isModalOpen}
+          onClose={handleCloseModal}
+        />
+      )}
     </section>
   );
 }
