1c8ec03f29cd1691c784cad5a524cfcbd5757334
Matheus Torsoni de Campos <142530928+tcmatheus@users.noreply.github.com>
2024-11-29 00:18:28 -0300

    Update firebase-config.js
    add config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAy3widLMh_An-DrhGvt0QMlj0-3carc9s",
-  authDomain: "sou-drop.firebaseapp.com",
-  projectId: "sou-drop",
-  storageBucket: "sou-drop.appspot.com",
-  messagingSenderId: "684597558004",
-  appId: "1:684597558004:web:11bb2d287bea8b44873ca1",
-  measurementId: "G-NDTV1673PX"
+  apiKey: "sua_api_key",
+  authDomain: "domain",
+  projectId: "project_id",
+  storageBucket: "storageBucket",
+  messagingSenderId: "....",
+  appId: "...",
+  measurementId: "..."
 };
 
 
